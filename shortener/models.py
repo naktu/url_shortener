fd8d861779b6ed9299a747271fea5d4cbebdcd8e
tutunak <dk@tutunak.com>
2017-08-15 17:43:20 +0300

    added simple model for our site

--- shortener/models.py
+++ shortener/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+
 class ShortURL(models.Model):
-    url = models.CharField(max_length=2000)
+    url = models.URLField(max_length=2000)
+    short_code = models.CharField(max_length=15, unique=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.url)
