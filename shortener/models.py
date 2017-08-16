658b7cb560f2a6f71fdcb441a8fe1c173d8b8273
tutunak <dk@tutunak.com>
2017-08-16 14:43:14 +0300

    update shortcode

--- shortener/models.py
+++ shortener/models.py
@@ -1,20 +1,17 @@
-import random
-
-from string import ascii_letters, digits
 from django.db import models
+from .utils import create_shortcode
 
-def code_generator(size=7, chars=ascii_letters+digits):
-    return ''.join(random.choice(chars) for _ in range(size))
 
 class ShortURL(models.Model):
     url = models.URLField(max_length=2000)
-    short_code = models.CharField(max_length=15, unique=True)
+    short_code = models.CharField(max_length=15, unique=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
-        self.short_code = code_generator()
-        super().save(*args, **kwargs)
+        if self.short_code is None or self.short_code == "":
+            self.short_code = create_shortcode(self)
+            super().save(*args, **kwargs)
 
     def __str__(self):
         return str(self.url)
---
+++ shortener/utils.py
@@ -0,0 +1,16 @@
+import random
+
+from string import ascii_letters, digits
+
+
+def code_generator(size=7, chars=ascii_letters+digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def create_shortcode(instance, size=7):
+    new_code = code_generator(size=size)
+    s_class = instance.__class__
+    qs_exists = s_class.object.filter(shotcode=new_code).exists()
+    if qs_exists:
+        return create_shortcode(instance, size=size)
+    return new_code
